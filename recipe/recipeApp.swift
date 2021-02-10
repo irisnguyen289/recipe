736ca14abbb536efce2584c2ded823be9852e555
Iris Nguyen <irisnguyen289@gmail.com>
2021-02-10 03:05:24 -0600

    Initial Commit

---
+++ recipe/recipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  recipeApp.swift
+//  recipe
+//
+//  Created by mac on 2/10/21.
+//
+
+import SwiftUI
+
+@main
+struct recipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
